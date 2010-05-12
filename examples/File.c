6c71d8ccceb15cd532b04a244b112eed3759f45d
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-05-12 22:47:18 +0200

    examples: Add example code for File module.

---
+++ examples/File.c
@@ -0,0 +1,32 @@
+#include <File.h>
+
+ExceptionManager exc;
+
+int main(void) {
+	ExceptionManager_Init(&exc);
+
+	File0(&exc);
+	String0(&exc);
+
+	File file;
+
+	try (&exc) {
+		File_Open(&file, String("File.txt"), O_RDONLY);
+	} catch(&File_NotFoundException, e) {
+		String_Print(String("File not found.\n"));
+		return EXIT_FAILURE;
+	} finally {
+
+	} tryEnd;
+
+	String s = StackString(5);
+
+	do {
+		s.len = File_Read(&file, s.buf, s.size);
+		String_Print(s);
+	} while (s.len > 0);
+
+	File_Close(&file);
+
+	return EXIT_SUCCESS;
+}
