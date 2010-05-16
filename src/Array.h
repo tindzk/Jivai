ab455f4cbe9283cf455814a0dc93f3e38f24d282
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-05-16 16:07:24 +0200

    Array: Refactored macros.
    Improve Array_Resize() for the ability to specify a callback.
    
    Additionally, introduce Array_Reset() as an alternative to
    Array_Resize() when shrinking the allocated memory is not
    desired.

--- src/Array.h
+++ src/Array.h
@@ -21,14 +21,32 @@ void Array_CustomAlign(Array *this, size_t len, size_t size);
 #define Array_Init(this, cnt) \
 	Array_CustomInit((void *) this, cnt, sizeof(typeof((this)->buf[0])))
 
-#define Array_Destroy(this, freeItem)          \
-	for (size_t i = 0; i < (this)->len; i++) { \
-		freeItem(&(this)->buf[i]);             \
-	}                                          \
-	Array_CustomDestroy((void *) this);
+#define Array_ResetRange(this, offset, cnt, freeItem)    \
+	do {                                                 \
+		for (size_t i = offset; i < offset + cnt; i++) { \
+			freeItem(&(this)->buf[i]);                   \
+		}                                                \
+		(this)->len -= cnt;                              \
+	} while(0)
 
-#define Array_Resize(this, cnt) \
-	Array_CustomResize((void *) this, cnt, sizeof(typeof((this)->buf[0])))
+#define Array_Reset(this, cnt, freeItem) \
+	Array_ResetRange(this, (this)->len - cnt, cnt, freeItem)
+
+#define Array_Destroy(this, freeItem)                     \
+	do {                                                  \
+		Array_ResetRange(this, 0, (this)->len, freeItem); \
+		Array_CustomDestroy((void *) this);               \
+	} while(0)
+
+#define Array_Resize(this, cnt, freeItem)      \
+	do {                                       \
+		if ((this)->len > cnt) {               \
+			Array_ResetRange(this, cnt,        \
+				(this)->len - cnt, freeItem);  \
+		}                                      \
+		Array_CustomResize((void *) this, cnt, \
+			sizeof(typeof((this)->buf[0])));   \
+	} while(0)
 
 #define Array_Align(this, cnt) \
 	Array_CustomAlign((void *) this, cnt, sizeof(typeof((this)->buf[0])))
