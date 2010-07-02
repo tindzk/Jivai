d32b7c9f9fa96d93285fef0c5e76288cee980505
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-07-02 14:36:32 +0200

    FileStream: Fix regression introduced by 0d3b9127e476a486cba2b8e02b6435beb1e3cfef.
    This is needed due to a Clang limitation: While compiling C code,
    overloaded functions cannot be used as pointers. This patch adds an
    intermediate function which circumvents the limitation.

--- src/FileStream.c
+++ src/FileStream.c
@@ -1,7 +1,12 @@
 #include "FileStream.h"
 
+/* Clang does not support pointers to overloaded C functions.  */
+static void __write(File *this, void *buf, size_t len) {
+	File_Write(this, buf, len);
+}
+
 StreamInterface FileStream_Methods = {
 	.read  = (void *) FileStream_Read,
-	.write = (void *) FileStream_Write,
+	.write = (void *) __write,
 	.close = (void *) FileStream_Close
 };
