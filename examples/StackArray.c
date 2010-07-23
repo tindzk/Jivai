20e4c92f9d74e1e0fa05369e5c28e1a427f91ee7
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-07-23 15:49:28 +0200

    Add `StackArray' module.

---
+++ examples/StackArray.c
@@ -0,0 +1,49 @@
+#include <String.h>
+#include <StackArray.h>
+
+ExceptionManager exc;
+
+typedef struct {
+	String field1;
+	String field2;
+} Item;
+
+void DestroyItem(Item *item) {
+	String_Destroy(&item->field1);
+	String_Destroy(&item->field2);
+}
+
+int main(void) {
+	ExceptionManager_Init(&exc);
+
+	String0(&exc);
+
+	StackArray(arr, Item, 100);
+
+	StackArray_Init(arr, Item);
+
+	for (size_t i = 0; i < 150; i++) {
+		if (!StackArray_CanPush(arr)) {
+			break;
+		}
+
+		Item item = {
+			String_Clone(Integer_ToString(i)),
+			String_Clone(Integer_ToString(i + 5))
+		};
+
+		StackArray_Push(arr, item, Item);
+	}
+
+	Item *buf = StackArray_GetBuffer(arr, Item);
+
+	for (size_t i = 0; i < arr->len; i++) {
+		String_FmtPrint(String("%:%\n"),
+			buf[i].field1,
+			buf[i].field2);
+	}
+
+	StackArray_Foreach(arr, DestroyItem, Item);
+
+	return EXIT_SUCCESS;
+}
---
+++ src/StackArray.h
@@ -0,0 +1,71 @@
+#ifndef STACK_ARRAY_H
+#define STACK_ARRAY_H
+
+typedef struct {
+	size_t len;
+	size_t size;
+	void *buf[0];
+} StackArray;
+
+/* Calculates the number of `StackArray' instances being necessary
+ * to hold one item.
+ */
+#define StackArray_OneItem(item)                        \
+	((sizeof(item) <= sizeof(StackArray))               \
+	 ? sizeof(StackArray)                               \
+	 : (((sizeof(item) % sizeof(StackArray)) == 0)      \
+		 ?   (sizeof(item) / sizeof(StackArray))        \
+		 :  ((sizeof(item) / sizeof(StackArray)) + 1)))
+
+/* Same as StackArray_OneItem() but the result fit for `len' items. */
+#define StackArray_ManyItems(item, len)                \
+	(((sizeof(item) * len) / sizeof(StackArray)) + (   \
+	(((sizeof(item) * len) % sizeof(StackArray)) != 0) \
+		? StackArray_OneItem(item)                     \
+		: 0))
+
+/* Calculates the final size ensuring that sufficient space for the
+ * StackArray headers `len' and `size' is available.
+ */
+#define StackArray_CalcSize(item, len)                                  \
+	(StackArray_ManyItems(item, len) +                                  \
+		((((sizeof(StackArray) + sizeof(item) * len) -                  \
+		   (StackArray_ManyItems(item, len)) * sizeof(StackArray)) > 0) \
+			? 1                                                         \
+			: 0))
+
+#define StackArray(name, item, len) \
+	StackArray name[StackArray_CalcSize(item, len)]
+
+#define StackArray_GetSize(this, item) \
+	(sizeof(this) / sizeof(item))
+
+#define StackArray_Init(this, item)                    \
+	do {                                               \
+		(this)->len  = 0;                              \
+		(this)->size = StackArray_GetSize(this, item); \
+	} while(0)
+
+#define StackArray_GetBuffer(this, item) \
+	((item *) &(this)->buf[0])
+
+#define StackArray_At(this, i, item) \
+	StackArray_GetBuffer(this, item)[i]
+
+#define StackArray_Foreach(this, cb, item)         \
+	do {                                           \
+		for (size_t i = 0; i < (this)->len; i++) { \
+			cb(&StackArray_At(this, i, item));     \
+		}                                          \
+	} while(0)
+
+#define StackArray_CanPush(this) \
+	(((this)->size != 0 && ((this)->len + 1) <= (this)->size))
+
+#define StackArray_Push(this, data, item)              \
+	do {                                               \
+		StackArray_At(this, (this)->len, item) = data; \
+		(this)->len++;                                 \
+	} while(0)
+
+#endif
