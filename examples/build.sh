61765a363510cc533ac3c87907d80c033e3cb03d
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-08-23 14:05:03 +0200

    examples/: Add build.sh for compiling faster.

---
+++ examples/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+mkdir -p build/Jivai
+
+if [ -z "$1" ]
+then
+	echo "No parameter given."
+	exit 1
+fi
+
+../../Jivai-Utils/jutils.bin \
+	build                    \
+	output=build/$1.bin      \
+	main=$1.c                \
+	include=../src           \
+	map=.:build              \
+	map=../src:build/Jivai   \
+	optimlevel=0             \
+	dbgsym=yes               \
+	inclhdr=config.h         \
+	link=@bfd
