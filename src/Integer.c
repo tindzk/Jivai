ea578f16fb4a06309c3e7506f8a2120360b5c358
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-10-04 19:24:45 +0200

    Integer: Improve macros.

--- src/Integer.c
+++ src/Integer.c
@@ -64,35 +64,34 @@ size_t Integer64_CountDigits(int64_t num) {
 	INTEGER_COUNT_DIGITS(num)
 }
 
-#define INTEGER_TO_STRING_BUF(num, res, digits, type)        \
-	type orig = num;                                         \
-	size_t digit = digits;                                   \
-	int offset;                                              \
-	if (num < 0) {                                           \
-		res.buf[0] = '-';                                    \
-		offset = 1;                                          \
-		orig *= -1;                                          \
-	} else {                                                 \
-		offset = 0;                                          \
-	}                                                        \
-	/* Force new length. */                                  \
-	res.len = offset + digit;                                \
-	/* Now operate on the original number. */                \
-	do {                                                     \
-		/* Necessary as indexes start with 0. */             \
-		digit--;                                             \
-		/* Convert current ones digit (48 = (int) '0'). */   \
-		res.buf[offset + digit] = (char) ((orig % 10) + 48); \
-		/* Remove current ones digit. */                     \
-		orig /= 10;                                          \
-	} while(orig);                                           \
-	return res;
+#define INTEGER_TO_STRING_BUF(num, res, digits, type)         \
+	type orig = num;                                          \
+	size_t digit = digits;                                    \
+	int offset;                                               \
+	if (num < 0) {                                            \
+		res->buf[0] = '-';                                    \
+		offset = 1;                                           \
+		orig *= -1;                                           \
+	} else {                                                  \
+		offset = 0;                                           \
+	}                                                         \
+	/* Force new length. */                                   \
+	res->len = offset + digit;                                \
+	/* Now operate on the original number. */                 \
+	do {                                                      \
+		/* Necessary as indexes start with 0. */              \
+		digit--;                                              \
+		/* Convert current ones digit (48 = (int) '0'). */    \
+		res->buf[offset + digit] = (char) ((orig % 10) + 48); \
+		/* Remove current ones digit. */                      \
+		orig /= 10;                                           \
+	} while(orig);
 
-String Integer_ToStringBuf(int num, String res) {
+void Integer_ToStringBuf(int num, String *res) {
 	INTEGER_TO_STRING_BUF(num, res, Integer_CountDigits(num), int)
 }
 
-String Integer64_ToStringBuf(int64_t num, String res) {
+void Integer64_ToStringBuf(int64_t num, String *res) {
 	INTEGER_TO_STRING_BUF(num, res, Integer64_CountDigits(num), int64_t)
 }
 
--- src/Integer.h
+++ src/Integer.h
@@ -21,8 +21,8 @@ int64_t Integer64_ParseString(String s);
 size_t Integer_CountDigits(int num);
 size_t Integer64_CountDigits(int64_t num);
 
-String Integer_ToStringBuf(int num, String buf);
-String Integer64_ToStringBuf(int64_t num, String buf);
+void Integer_ToStringBuf(int num, String *buf);
+void Integer64_ToStringBuf(int64_t num, String *buf);
 
 short Integer_Compare(int a, int b);
 short Integer64_Compare(int64_t a, int64_t b);
@@ -30,8 +30,12 @@ short Integer64_Compare(int64_t a, int64_t b);
 #define Integer_Length   String("-2147483647").len
 #define Integer64_Length String("-9223372036854775807").len
 
-#define Integer_ToString(num) \
-	Integer_ToStringBuf(num, StackString(Integer_Length))
+#define Integer_ToString(num) ({              \
+	String tmp = StackString(Integer_Length); \
+	Integer_ToStringBuf(num, &tmp);           \
+	tmp; })
 
-#define Integer64_ToString(num) \
-	Integer64_ToStringBuf(num, StackString(Integer64_Length))
+#define Integer64_ToString(num) ({              \
+	String tmp = StackString(Integer64_Length); \
+	Integer64_ToStringBuf(num, &tmp);           \
+	tmp; })
