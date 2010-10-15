58fffe24dbb03f93e01278345b1382bc3c9f4453
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-10-15 12:11:42 +0200

    Char: Be more consistent.

--- src/Char.c
+++ src/Char.c
@@ -5,36 +5,36 @@ void Char_Print(char c) {
 }
 
 /* Taken from diet libc (dietlibc-0.32/lib/isalpha.c). */
-bool Char_IsAlpha(int ch) {
-	return (unsigned int) ((ch | 0x20) - 'a') < 26u;
+bool Char_IsAlpha(s32 ch) {
+	return (u32) ((ch | 0x20) - 'a') < 26u;
 }
 
 /* Taken from diet libc (dietlibc-0.32/lib/tolower.c). */
-int Char_ToLower(int c) {
-	if (Char_IsUpper(c)) {
-		c += 'a' - 'A';
+s32 Char_ToLower(s32 ch) {
+	if (Char_IsUpper(ch)) {
+		ch += 'a' - 'A';
 	}
 
-	return c;
+	return ch;
 }
 
 /* Taken from diet libc (dietlibc-0.32/lib/toupper.c). */
-int Char_ToUpper(int c) {
-	if (Char_IsLower(c)) {
-		c += 'A' - 'a';
+s32 Char_ToUpper(s32 ch) {
+	if (Char_IsLower(ch)) {
+		ch += 'A' - 'a';
 	}
 
-	return c;
+	return ch;
 }
 
 /* Taken from diet libc (dietlibc-0.32/liblatin1/latin1-isprint.c). */
-bool Char_IsPrintable(int x) {
-	unsigned char c = x & 0xff;
+bool Char_IsPrintable(s32 ch) {
+	u8 c = ch & 0xff;
 	return (c >= 32 && c <= 126) || (c >= 160);
 }
 
 /* Taken from diet libc (dietlibc-0.32/lib/isxdigit.c). */
-bool Char_IsHexDigit(int ch) {
-	return (size_t)( ch         - '0') < 10u
-		|| (size_t)((ch | 0x20) - 'a') <  6u;
+bool Char_IsHexDigit(s32 ch) {
+	return (u32)( ch         - '0') < 10u
+		|| (u32)((ch | 0x20) - 'a') <  6u;
 }
--- src/Char.h
+++ src/Char.h
@@ -1,4 +1,3 @@
-#import "Types.h"
 #import "Kernel.h"
 
 /* See also: http://php.net/manual/en/function.trim.php */
@@ -13,15 +12,15 @@
 
 /* Taken from diet libc (dietlibc-0.32/lib/tolower.c). */
 #define Char_IsLower(c) \
-	((size_t) ((c) - 'a') < 26u)
+	((u32) ((c) - 'a') < 26u)
 
 /* Taken from diet libc (dietlibc-0.32/lib/toupper.c). */
 #define Char_IsUpper(c) \
-	((size_t) ((c) - 'A') < 26u)
+	((u32) ((c) - 'A') < 26u)
 
 void Char_Print(char c);
-bool Char_IsAlpha(int ch);
-int Char_ToLower(int c);
-int Char_ToUpper(int c);
-bool Char_IsPrintable(int x);
-bool Char_IsHexDigit(int ch);
+bool Char_IsAlpha(s32 ch);
+s32 Char_ToLower(s32 ch);
+s32 Char_ToUpper(s32 ch);
+bool Char_IsPrintable(s32 ch);
+bool Char_IsHexDigit(s32 ch);
