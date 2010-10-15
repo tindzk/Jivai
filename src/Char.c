addb2929d51bb3f6b2a61a717d150a649ae1255a
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-10-15 12:18:18 +0200

    Char: Move macros to functions.

--- src/Char.c
+++ src/Char.c
@@ -10,6 +10,16 @@ bool Char_IsAlpha(s32 ch) {
 }
 
 /* Taken from diet libc (dietlibc-0.32/lib/tolower.c). */
+bool Char_IsLower(s32 ch) {
+	return (u32) (ch - 'a') < 26u;
+}
+
+/* Taken from diet libc (dietlibc-0.32/lib/toupper.c). */
+bool Char_IsUpper(s32 ch) {
+	return (u32) (ch - 'A') < 26u;
+}
+
+/* Taken from diet libc (dietlibc-0.32/lib/tolower.c). */
 s32 Char_ToLower(s32 ch) {
 	if (Char_IsUpper(ch)) {
 		ch += 'a' - 'A';
@@ -37,4 +47,22 @@ bool Char_IsPrintable(s32 ch) {
 bool Char_IsHexDigit(s32 ch) {
 	return (u32)( ch         - '0') < 10u
 		|| (u32)((ch | 0x20) - 'a') <  6u;
+}
+
+/* See also: http://php.net/manual/en/function.trim.php */
+bool Char_IsSpace(char c) {
+	return c == ' '
+		|| c == '\t'
+		|| c == '\n'
+		|| c == '\r'
+		|| c == '\0'
+		|| c == '\x0B';
+}
+
+bool Char_IsDigit(char c) {
+	return c >= '0' && c <= '9';
+}
+
+short Char_ParseDigit(char c) {
+	return (short) (c - 48);
 }
--- src/Char.h
+++ src/Char.h
@@ -1,26 +1,13 @@
 #import "Kernel.h"
 
-/* See also: http://php.net/manual/en/function.trim.php */
-#define Char_IsSpace(c) \
-	((c) == ' ' || (c) == '\t' || (c) == '\n' || (c) == '\r' || (c) == '\0' || (c) == '\x0B')
-
-#define Char_IsDigit(c) \
-	(((c) >= '0') && ((c) <= '9'))
-
-#define Char_ParseDigit(c) \
-	(short) ((c) - 48)
-
-/* Taken from diet libc (dietlibc-0.32/lib/tolower.c). */
-#define Char_IsLower(c) \
-	((u32) ((c) - 'a') < 26u)
-
-/* Taken from diet libc (dietlibc-0.32/lib/toupper.c). */
-#define Char_IsUpper(c) \
-	((u32) ((c) - 'A') < 26u)
-
 void Char_Print(char c);
 bool Char_IsAlpha(s32 ch);
+bool Char_IsLower(s32 ch);
+bool Char_IsUpper(s32 ch);
 s32 Char_ToLower(s32 ch);
 s32 Char_ToUpper(s32 ch);
 bool Char_IsPrintable(s32 ch);
 bool Char_IsHexDigit(s32 ch);
+bool Char_IsSpace(char c);
+bool Char_IsDigit(char c);
+short Char_ParseDigit(char c);
