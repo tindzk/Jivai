9dbdcfd721aa50ce2d5d58198ae341422dbd2f72
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-10-15 11:36:53 +0200

    Types: Declare more integer types.

--- src/Types.h
+++ src/Types.h
@@ -17,6 +17,15 @@ typedef enum {
 	((void *) 0)
 #endif
 
+typedef   signed char       s8;
+typedef unsigned char       u8;
+typedef   signed short int s16;
+typedef unsigned short int u16;
+typedef   signed int       s32;
+typedef unsigned int       u32;
+typedef   signed long long s64;
+typedef unsigned long long u64;
+
 #if defined(__x86_64__)
 	typedef unsigned long  size_t;
 	typedef   signed long  ssize_t;
