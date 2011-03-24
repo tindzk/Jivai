40651a0e37dc179a1e46c71311d26462b31c630b
Tim Nieradzik <tim.nieradzik@gmx.de>
2011-03-24 18:41:59 +0100

    examples/File: Fix code.

--- examples/File.c
+++ examples/File.c
@@ -6,23 +6,29 @@ int main(void) {
 
 	try {
 		File_Open(&file, $("File.txt"), FileStatus_ReadOnly);
-	} clean catch (File, NotFound) {
+	} catch (File, NotFound) {
 		String_Print($("File not found.\n"));
 		excReturn ExitStatus_Failure;
 	} finally {
 
 	} tryEnd;
 
-	String s = StackString(5);
+	String s = String_New(5);
 
 	do {
-		s.len = File_Read(&file, s.buf, s.size);
-		String_Print(s);
+		s.len = File_Read(&file, s.buf, String_GetSize(s));
+		String_Print(s.rd);
 	} while (s.len > 0);
 
+	String_Destroy(&s);
+
+	String strSize = Integer_ToString(File_GetSize(&file));
+
 	String_Print($("size: "));
-	String_Print(Integer_ToString(File_GetSize(&file)));
+	String_Print(strSize.rd);
 	String_Print($("\n"));
+
+	String_Destroy(&strSize);
 
 	File_Close(&file);
 
