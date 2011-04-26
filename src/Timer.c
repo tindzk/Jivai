a91e6245134099e449d7fd4391be788af4bc5244
Tim Nieradzik <tim.nieradzik@gmx.de>
2011-04-26 20:56:33 +0200

    Timer: Fix regression.

--- src/Timer.c
+++ src/Timer.c
@@ -102,7 +102,7 @@ def(Task *, AsTask, ref(OnTimer) onTimer) {
 
 	data->cb    = onTimer;
 	data->timer = *this;
-	data->entry = EventLoop_AddChannel(EventLoop_GetInstance(), &this->ch,
+	data->entry = EventLoop_AddChannel(EventLoop_GetInstance(), &data->timer.ch,
 		EventLoop_OnInput_For(data, ref(OnInvoke)));
 
 	return task;
