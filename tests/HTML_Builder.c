8d95dbe1b8c351245cee59c21edd6d70f41bc114
Tim Nieradzik <tim.nieradzik@gmx.de>
2011-05-19 11:13:37 +0200

    tests/HTML_Builder: Fix regression.

--- tests/HTML_Builder.c
+++ tests/HTML_Builder.c
@@ -28,7 +28,7 @@ tsDestroy {
 	String_Destroy(&this->s);
 }
 
-overload def(void, Process, HTML_TokenType type, RdString value) {
+overload def(void, Process, XML_TokenType type, RdString value) {
 	if (this->clear) {
 		StringStream_Clear(&this->stream);
 		this->clear = false;
@@ -36,12 +36,12 @@ overload def(void, Process, HTML_TokenType type, RdString value) {
 
 	HTML_Builder_ProcessToken(&this->html, type, value);
 
-	if (type == HTML_TokenType_Done) {
+	if (type == XML_TokenType_Done) {
 		this->clear = true;
 	}
 }
 
-overload def(void, Process, HTML_TokenType type) {
+overload def(void, Process, XML_TokenType type) {
 	call(Process, type, $(""));
 }
 
@@ -50,104 +50,104 @@ def(bool, Equals, RdString s) {
 }
 
 tsCase(Acute, "Value") {
-	call(Process, HTML_TokenType_Value, $("  Hello World!  "));
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_Value, $("  Hello World!  "));
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("  Hello World!  ")));
 
-	call(Process, HTML_TokenType_Value, $("  "));
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_Value, $("  "));
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("  ")));
 }
 
 tsCase(Acute, "Comments") {
-	call(Process, HTML_TokenType_Comment, $(""));
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_Comment, $(""));
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("<!---->")));
 
-	call(Process, HTML_TokenType_Comment, $("Comment"));
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_Comment, $("Comment"));
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("<!--Comment-->")));
 
-	call(Process, HTML_TokenType_Comment, $(" Comment "));
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_Comment, $(" Comment "));
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("<!-- Comment -->")));
 
-	call(Process, HTML_TokenType_Comment, $("[if IE]><link rel=\"stylesheet\" /><![endif]"));
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_Comment, $("[if IE]><link rel=\"stylesheet\" /><![endif]"));
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("<!--[if IE]><link rel=\"stylesheet\" /><![endif]-->")));
 }
 
 tsCase(Acute, "Tags (simple)") {
-	call(Process, HTML_TokenType_TagStart, $("br"));
-	call(Process, HTML_TokenType_TagEnd, $("br"));
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_TagStart, $("br"));
+	call(Process, XML_TokenType_TagEnd, $("br"));
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("<br></br>")));
 
-	call(Process, HTML_TokenType_TagStart, $("br"));
-	call(Process, HTML_TokenType_Option, $("option"));
-	call(Process, HTML_TokenType_TagEnd, $("br"));
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_TagStart, $("br"));
+	call(Process, XML_TokenType_Option, $("option"));
+	call(Process, XML_TokenType_TagEnd, $("br"));
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("<br option></br>")));
 }
 
 tsCase(Acute, "Tags (XHTML)") {
-	call(Process, HTML_TokenType_TagStart, $("br"));
-	call(Process, HTML_TokenType_TagEnd);
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_TagStart, $("br"));
+	call(Process, XML_TokenType_TagEnd);
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("<br />")));
 
-	call(Process, HTML_TokenType_TagStart, $("br"));
-	call(Process, HTML_TokenType_Option, $("option"));
-	call(Process, HTML_TokenType_TagEnd);
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_TagStart, $("br"));
+	call(Process, XML_TokenType_Option, $("option"));
+	call(Process, XML_TokenType_TagEnd);
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("<br option />")));
 }
 
 tsCase(Acute, "Attributes") {
-	call(Process, HTML_TokenType_TagStart, $("input"));
-	call(Process, HTML_TokenType_AttrName, $("name"));
-	call(Process, HTML_TokenType_AttrValue, $("text"));
-	call(Process, HTML_TokenType_TagEnd);
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_TagStart, $("input"));
+	call(Process, XML_TokenType_AttrName, $("name"));
+	call(Process, XML_TokenType_AttrValue, $("text"));
+	call(Process, XML_TokenType_TagEnd);
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("<input name=text />")));
 
-	call(Process, HTML_TokenType_TagStart, $("input"));
-	call(Process, HTML_TokenType_AttrName, $("name"));
-	call(Process, HTML_TokenType_AttrValue, $("text"));
-	call(Process, HTML_TokenType_TagEnd, $("input"));
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_TagStart, $("input"));
+	call(Process, XML_TokenType_AttrName, $("name"));
+	call(Process, XML_TokenType_AttrValue, $("text"));
+	call(Process, XML_TokenType_TagEnd, $("input"));
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("<input name=text></input>")));
 
-	call(Process, HTML_TokenType_TagStart, $("input"));
-	call(Process, HTML_TokenType_AttrName, $("name"));
-	call(Process, HTML_TokenType_AttrValue, $("text"));
-	call(Process, HTML_TokenType_Value, $("Value"));
-	call(Process, HTML_TokenType_TagEnd, $("input"));
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_TagStart, $("input"));
+	call(Process, XML_TokenType_AttrName, $("name"));
+	call(Process, XML_TokenType_AttrValue, $("text"));
+	call(Process, XML_TokenType_Value, $("Value"));
+	call(Process, XML_TokenType_TagEnd, $("input"));
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("<input name=text>Value</input>")));
 }
 
 tsCase(Acute, "Nesting") {
-	call(Process, HTML_TokenType_Value, $("value"));
-	call(Process, HTML_TokenType_TagStart, $("p"));
-	call(Process, HTML_TokenType_TagStart, $("b"));
-	call(Process, HTML_TokenType_Value, $("parag"));
-	call(Process, HTML_TokenType_TagEnd, $("b"));
-	call(Process, HTML_TokenType_TagEnd, $("p"));
-	call(Process, HTML_TokenType_Done);
+	call(Process, XML_TokenType_Value, $("value"));
+	call(Process, XML_TokenType_TagStart, $("p"));
+	call(Process, XML_TokenType_TagStart, $("b"));
+	call(Process, XML_TokenType_Value, $("parag"));
+	call(Process, XML_TokenType_TagEnd, $("b"));
+	call(Process, XML_TokenType_TagEnd, $("p"));
+	call(Process, XML_TokenType_Done);
 
 	Assert($("Equals"), call(Equals, $("value<p><b>parag</b></p>")));
 }
