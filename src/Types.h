5b4f971c392fa615302e9f89d01375b9b1b07730
Tim Nieradzik <tim.nieradzik@gmx.de>
2011-05-25 18:48:33 +0200

    Types: Add types.

--- src/Types.h
+++ src/Types.h
@@ -15,12 +15,25 @@ typedef unsigned int       u32;
 typedef   signed long long s64;
 typedef unsigned long long u64;
 
+typedef   signed char  sbyte;
+typedef unsigned char  ubyte;
+
 #if defined(__x86_64__)
 	typedef unsigned long  size_t;
 	typedef   signed long  ssize_t;
+	typedef   signed long  IntPtr;
+	typedef   signed int   shalf;
+	typedef unsigned int   uhalf;
+	typedef   signed long  sword;
+	typedef unsigned long  uword;
 #else
 	typedef unsigned int   size_t;
 	typedef   signed int   ssize_t;
+	typedef   signed int   IntPtr;
+	typedef   signed short shalf;
+	typedef unsigned short uhalf;
+	typedef   signed int   sword;
+	typedef unsigned int   uword;
 #endif
 
 /* 16/32/64 bit big-endian representation. */
