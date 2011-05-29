e859e95563d485d4858b44d05691017966af3fbc
Tim Nieradzik <tim.nieradzik@gmx.de>
2011-05-29 13:37:15 +0200

    Depend reads parameters from file.

--- examples/Build.sh
+++ examples/Build.sh
@@ -8,16 +8,14 @@ then
 	exit 1
 fi
 
-../../Depend/Depend.exe    \
-	build                  \
-	output=build/$1.exe    \
-	manifest=Manifest.h    \
-	main=$1.c              \
-	include=.              \
-	include=../src         \
-	map=.:build            \
-	map=../src:build/Jivai \
-	optimlevel=0           \
-	dbgsym=yes             \
-	debug=yes              \
-	inclhdr=../config.h
+echo "output=build/$1.exe
+manifest=Manifest.h
+main=$1.c
+include=.
+include=../src
+map=.:build
+map=../src:build/Jivai
+optimlevel=0
+dbgsym=yes
+debug=yes
+inclhdr=../config.h" | ../../Depend/Depend.exe build /dev/stdin
