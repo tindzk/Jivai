c49460e0f3fd95636b1f4bd6abad59a65126461b
Tim Nieradzik <tim.nieradzik@gmx.de>
2011-07-11 17:28:41 +0200

    tests/Folder: Extend test case.

--- tests/Folder.c
+++ tests/Folder.c
@@ -16,13 +16,19 @@ tsCase(Acute, "Listing") {
 	Folder folder = Folder_new($("../tests/"));
 
 	size_t iter = 0;
+	bool found = false;
 
 	Folder_Entry entry;
 	while (Folder_read(&folder, &entry)) {
+		if (String_Equals(entry.name, $("Folder.c"))) {
+			found = true;
+		}
+
 		iter++;
 	}
 
-	Assert($("Iterations"), iter > 1);
+	Assert($("Found"),      found);     /* Found this file? */
+	Assert($("Iterations"), iter > 10); /* Approximation.   */
 	Assert($("Not empty"),  entry.name.len > 0);
 
 	Folder_destroy(&folder);
