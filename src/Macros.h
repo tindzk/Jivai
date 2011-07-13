1513b84a144040a889229ad4f4dd029d55c21988
Tim Nieradzik <tim.nieradzik@gmx.de>
2011-07-13 15:52:05 +0200

    Macros: Fix seach().

--- src/Macros.h
+++ src/Macros.h
@@ -84,9 +84,11 @@ static alwaysInline int _useValue(int value) {
 		name < &(arr)->buf[(arr)->len];               \
 		name++)
 
-#define seach(i, name, arr) \
-	for (typeof((arr)->len) i = 0; ; ({ break; })) \
-	for (typeof((arr)->buf[0]) name = (arr)->buf[i]; i < (arr)->len; i++)
+#define seach(i, name, arr)                                  \
+	for (typeof((arr)->len) i = 0; ; ({ break; }))           \
+	for (typeof((arr)->buf[0]) name;                         \
+		i < (arr)->len && ({ name = (arr)->buf[i]; true; }); \
+		i++)
 
 #define getIndex(name, arr) \
 	(size_t) ((name) - (arr)->buf)
